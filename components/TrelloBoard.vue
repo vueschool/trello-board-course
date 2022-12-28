f34d183afa086b3b78f16fba45c3e10f9299e7e0
Daniel Kelly <me@danielkelly.io>
2022-12-28 10:43:22 -0600

    4-provide-the-board-data-and-structure-the-html

---
+++ components/TrelloBoard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { Column } from "~~/types";
+import { nanoid } from "nanoid";
+const columns = ref<Column[]>([
+  {
+    title: "Backlog",
+    id: nanoid(),
+    tasks: [
+      {
+        title: "Create marketing landing page",
+        createdAt: new Date(),
+        id: nanoid(),
+      },
+      {
+        title: "Develop cool new feature",
+        createdAt: new Date(),
+        id: nanoid(),
+      },
+      { title: "Fix page nav bug", createdAt: new Date(), id: nanoid() },
+    ],
+  },
+  { title: "Selected for Dev", id: nanoid(), tasks: [] },
+  { title: "In Progress", id: nanoid(), tasks: [] },
+  { title: "QA", id: nanoid(), tasks: [] },
+  { title: "Complete", id: nanoid(), tasks: [] },
+]);
+</script>
+<template>
+  <div>
+    <div v-for="column in columns" :key="column.id">
+      <header>
+        {{ column.title }}
+      </header>
+      <p v-for="task in column.tasks" :key="task.id">
+        {{ task.title }}
+      </p>
+    </div>
+  </div>
+</template>
