f3b0d298ab1f094dd3305e8cb325899f6eca56cc
Daniel Kelly <me@danielkelly.io>
2023-01-09 12:21:49 -0600

    13-delete-tasks-with-backspace

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -52,7 +52,12 @@ const alt = useKeyModifier("Alt");
           >
             <template #item="{ element: task }: { element: Task }">
               <div>
-                <TrelloBoardTask :task="task" />
+                <TrelloBoardTask
+                  :task="task"
+                  @delete="
+                    column.tasks = column.tasks.filter((t) => t.id !== $event)
+                  "
+                />
               </div>
             </template>
           </draggable>
--- components/TrelloBoardTask.vue
+++ components/TrelloBoardTask.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-import type { Task } from "@/types";
-defineProps<{
+import type { Task, ID } from "@/types";
+const props = defineProps<{
   task: Task;
 }>();
+
+const emit = defineEmits<{
+  (e: "delete", payload: ID): void;
+}>();
+
+const focused = ref(false);
+onKeyStroke("Backspace", (e) => {
+  if (focused.value) emit("delete", props.task.id);
+});
 </script>
 <template>
   <div
     :title="task.createdAt.toLocaleString()"
     class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex"
+    @focus="focused = true"
+    @blur="focused = false"
+    tabindex="0"
   >
     <DragHandle class="pr-2" />
     <span>
@@ -27,5 +39,11 @@ defineProps<{
 .sortable-ghost .task::after {
   content: "";
   @apply absolute top-0 bottom-0 left-0 right-0 bg-slate-300 rounded;
+}
+
+.task:focus,
+.task:focus-visible {
+  @apply outline-gray-400 !important;
+  outline: gray auto 1px;
 }
 </style>
