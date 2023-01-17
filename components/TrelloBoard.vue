26ea10cfef744b3bf86c89f8461c6578d4dcf998
Daniel Kelly <me@danielkelly.io>
2023-01-17 14:30:26 -0600

    14-add-remove-columns

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -26,22 +26,48 @@ const columns = ref<Column[]>([
   { title: "Complete", id: nanoid(), tasks: [] },
 ]);
 const alt = useKeyModifier("Alt");
+
+function createColumn() {
+  const column: Column = {
+    id: nanoid(),
+    title: "",
+    tasks: [],
+  };
+  columns.value.push(column);
+  nextTick(() => {
+    (
+      document.querySelector(
+        ".column:last-of-type .title-input"
+      ) as HTMLInputElement
+    ).focus();
+  });
+}
 </script>
 <template>
-  <div>
+  <div class="flex items-start overflow-x-auto gap-4">
     <draggable
       v-model="columns"
       group="columns"
       :animation="150"
       handle=".drag-handle"
       item-key="id"
-      class="flex gap-4 overflow-x-auto items-start"
+      class="flex gap-4 items-start"
     >
       <template #item="{ element: column }: { element: Column }">
         <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
           <header class="font-bold mb-4">
             <DragHandle />
-            {{ column.title }}
+            <input
+              class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+              @keyup.enter="($event.target as HTMLInputElement).blur()"
+              @keydown.backspace="
+                column.title === ''
+                  ? (columns = columns.filter((c) => c.id !== column.id))
+                  : null
+              "
+              type="text"
+              v-model="column.title"
+            />
           </header>
           <draggable
             v-model="column.tasks"
@@ -68,5 +94,11 @@ const alt = useKeyModifier("Alt");
         </div>
       </template>
     </draggable>
+    <button
+      @click="createColumn"
+      class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50"
+    >
+      + Add Another Column
+    </button>
   </div>
 </template>
